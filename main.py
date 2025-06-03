36c7e7a43b1cb8736e08f193aac6ebd862ec6b0a
Fsehaye Medhanie <werkat41@gmail.com>
2025-06-02 22:18:35 -0700

    commit on 1:13 on the tutor-video on June-02-25

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 @app.get("/")
 def root():
