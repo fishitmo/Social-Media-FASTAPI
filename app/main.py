da5d08baa188d7b3e254ccb34ea8ea8f38784e06
Fsehaye Medhanie <werkat41@gmail.com>
2025-06-17 13:37:19 -0700

    update @4:31:21 , object relational Mapper(ORM)

--- app/main.py
+++ app/main.py
@@ -54,15 +54,20 @@ def get_posts():
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)  
-    return {"Data": post_dict}
+    cursor.execute(""" INSERT INTO posts (title, content, published) Values (%s, %s,%s) RETURNING *  """, (post.title, post.content, post.published))
+    
+    new_post = cursor.fetchone()
+    
+    conn.commit()
+    
+    return {"Data": new_post}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int):
-    post = find_post(id)
+    cursor.execute(""" SELECT * FROM posts WHERE id = %s""", (str(id),))
+    post = cursor.fetchone()
+    
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
         # response.status_code = status.HTTP_404_NOT_FOUND
@@ -71,25 +76,25 @@ def get_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    # delete post
-    # find the index in the array that has required id
-    # my_posts.pop(index)
-    index = find_index_post(id)
+   
+    cursor.execute("""DELETE FROM posts WHERE id = %s returning *""" , (str(id),))
+    delete_post = cursor.fetchone()
+    conn.commit()
     
-    if index is None:
+    if delete_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
     
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
+    cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
     
-    if index is None:
+    updated_post =cursor.fetchone()
+    conn.commit()
+    
+    if updated_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist")
     
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"Data": post_dict}
+    
+    return {"Data": updated_post}
