bc0bfc32235acfd7a02526582a10ef890df6a420
Fsehaye Medhanie <werkat41@gmail.com>
2025-07-09 16:23:02 -0700

    update @6:17:11, not to display the password of the user

--- app/main.py
+++ app/main.py
@@ -146,7 +146,7 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     
     return new_user
 
-@app.get("/users/{id}")
+@app.get("/users/{id}", response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
